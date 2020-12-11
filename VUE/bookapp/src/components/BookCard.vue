b00afa7760b05daf4059666a05bb6bd9c3b6b42d
Oleksii Skryba <Oleksii.Skryba@Gmail.Com>
2020-12-11 11:07:13 +0200

    reinstall bootstrap

---
+++ VUE/bookapp/src/components/BookCard.vue
@@ -0,0 +1,27 @@
+<!--BookCard.vue-->
+<template>
+    <div>
+        <div class="card">
+            <img :src="bookData.img_url" class="card-img-top">
+            <div class="card-body">
+                <p class="card-text">Title: {{bookData.title}}</p>
+                <p class="card-text">Author: {{bookData.author}}</p>
+                <p class="card-text">Description: {{bookData.description}}</p>
+                <slot></slot>
+                <slot name="button"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        bookData: {
+            type: Object,
+            required: true,
+            default: () => {}
+        }
+    }
+}
+</script>
---
+++ VUE/bookapp/src/components/BookList.vue
@@ -0,0 +1,64 @@
+<!--BookList.vue-->
+<template>
+    <div class="container">
+        <div>
+            <div class="row">
+                <div class="col-md-4">
+                    <BookCard />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BookCard from './BookCard.vue'
+
+export default {
+    components: {
+        BookCard
+    },
+     data(){
+        return {
+            books: [
+                {
+                    'author': 'David Deustch',
+                    'title': 'The beginning of Infinity',
+                    'description': 'A book on philosphy about the origins of man',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+                {
+                    'author': 'Paul Coelho',
+                    'title': 'The Alchemist',
+                    'description': 'The true search for one\'s treasure',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+                {
+                    'author': 'Susan Kaye Quinn',
+                    'title': 'Open Minds',
+                    'description': 'Sci-Fi Futuristic book about mind reading',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+                {
+                    'author': 'Robert Kiyosaki',
+                    'title': 'Rich Dad, Poor Dad',
+                    'description': 'Motivational book on wealth building',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+                {
+                    'author': 'Dan Brown',
+                    'title': 'The Da Vinci Code',
+                    'description': 'Conspiracy theories about secrets of the holy grail',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+                {
+                    'author': 'Arthur Hailey',
+                    'title': 'The money changers',
+                    'description': 'Travel back in time and experience the banking system',
+                    'img_url': 'https://placeimg.com/640/480/any'
+                },
+            ]
+        }
+     }
+}
+</script>
